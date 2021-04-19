710dda138598bd391229aa2858e09ab172f4e719
Henning Schw <henning.schwister@googlemail.com>
2021-04-19 13:34:37 +0200

    Update start-release.sh

--- scripts/start-release.sh
+++ scripts/start-release.sh
@@ -32,7 +32,7 @@ tar cfz medley/tmp/medley-$tag.tgz                        \
 cd medley
 
 echo making release
-echo ${{ secrets.GH_TOKEN }} >> .env
+echo ${{ secrets.DEPLOY }} >> .env
 gh auth login --with-token < .env
 sed s/'$tag'/$tag/g < release-notes.md > tmp/release-notes.md
 gh release create $tag -F tmp/release-notes.md -p -t $tag
